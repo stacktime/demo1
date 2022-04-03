a6c15320e45fc9868b62fb4ffdf5fd574a391457
wangyuanbo <wangyuanbo@nbugs.com>
2022-04-03 14:51:16 +0800

    2 模块引入

--- main/src/main/java/com/example/demo/Demo1Application.java
+++ main/src/main/java/com/example/demo/Demo1Application.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.example"})
+@SpringBootApplication
 public class Demo1Application {
 
     public static void main(String[] args) {
